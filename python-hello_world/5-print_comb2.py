72746721d88f0656eca000dce1a1bb7d5a60f469
danrealb <danrealb>
2023-07-19 16:40:11 -0700

    task 5

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -3,7 +3,7 @@
 #printing combination of number
 
 for number in range(0, 100):
-    if number ==90:
+    if number == 90:
         print("{}".format(number))
     else:
         print("{:02}".format(number), end=", ")
