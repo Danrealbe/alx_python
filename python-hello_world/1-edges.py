8b6da66152478a1a3cfe036a276d996ff7b0fa58
danrealb <danrealb>
2023-07-19 07:56:54 -0700

    single print

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
 # printing of single letter in python
-print("first 3 letters: {}".format(word[:3]))
-print("first 2 letters: {}".format(word[-2:]))
-print("middle word: {}".format(word[1: -1]))
+print("First 3 letters: {}".format(word[:3]))
+print("Last 2 letters: {}".format(word[-2:]))
+print("Middle word: {}".format(word[1: -1]))
