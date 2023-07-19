96e929bb761c08c1a635be1974984736e12cde34
danrealb <danrealb>
2023-07-19 15:21:09 -0700

    3 last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,7 +5,7 @@ digit = abs(number) % 10
 # soluction 
 if number >= 5:
         digit = - digit
-            print("Last digit of {} is {} and is".format(number, digit), end="")
+        print("Last digit of {} is {} and is".format(number, digit), end="")
                 print("{} the string is greater than 5".format(number))
             elif number == 0:
                     print("{} the string and is 0".format(number))
