b84fe1fc422064ab79452d09bf1cbd44a5e82f2c
danrealb <danrealb>
2023-07-23 06:32:27 -0700

    addition of two number: :

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    import add_0
+
+    add_0.add(1 + 2)
--- python-import_modules/0_add.py
+++
@@ -1,3 +0,0 @@
-import add_0
-
-add_0.add(1 + 2)
