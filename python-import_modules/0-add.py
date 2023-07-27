571135e3377471cc2fe7fd4c3648f0c1715df087
danrealb <danrealb>
2023-07-27 15:39:13 -0700

    correction

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
+if _name___== "_main_":
 from add_0 import add
 
-if __name__ == "__main__":
      a = 1
      b = 2   
     
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
