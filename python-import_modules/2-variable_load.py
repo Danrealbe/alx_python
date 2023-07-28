49e4a4bee407f6f70a7a06722319a910700af63e
danrealb <danrealb>
2023-07-28 11:08:04 -0700

    message

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+f __name__ == "__main__":
 
 a = 89
 
--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
  
-def safe_print_division(a, b):
-    try:
-        quot = a / b
-
-    except ZeroDivisionErro:
-        quot = None
-
-    finally:
-        print("Inside result: {}".format(quot))
-    return quot 
+ def safe_print_division(a, b):
+     try:
+         quot = a / b
+    
+    except ZeroDivisionError:
+         quot = None
+    
+    finally:    
+         print("Inside result: {}".format(quot))         
+     return quot
