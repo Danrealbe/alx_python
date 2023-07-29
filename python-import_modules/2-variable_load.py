0c1ee65e1de7538d4b675e5db9aa1cccd00a8de0
danrealb <danrealb>
2023-07-29 06:01:18 -0700

    exception message:

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
-a = 89
+    a = 89
 
 """Simple variable
 """
--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -3,7 +3,5 @@
 """raise exeption message""" 
 def raise_exception_msg(message=""):
 
-    x = -1 
-    if x < 0:
-        raise Exception("c is fun")
+    raise Nameerror(message)
 
