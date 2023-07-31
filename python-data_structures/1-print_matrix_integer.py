dc418ecfcb9353b65e189c2b14ddd31dd54d59bd
danrealb <danrealb>
2023-07-31 09:18:57 -0700

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -7,6 +7,6 @@ def print_matrix_integer(matrix=[[]]):
    # print a matrix of integers"""" 
     for i in range(len(matrix)):
         for j in range(len(matrix)):
-            print("{:d}".format(matrix[i][j]), end='')
+            print("{:d}" str.format(matrix[i][j]), end='')
             if j != (len(matrix[i]) -1):
                     print(" ", end="")
