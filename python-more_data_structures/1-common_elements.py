9e24825705212d9e4c215acec816c415ae85f73e
danrealb <danrealb>
2023-08-03 15:53:05 -0700

    commit

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3 
+
+# 1 - common element.py 
+
+def common_element(set_1, set_2):
+    """Return a set of common element in two sets."""
+
+    return (set_1 & set_2)
---
+++ python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+pythom more data structure
