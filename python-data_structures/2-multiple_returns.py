bafbb6b942a46d8606c022b60335082639fa9ae7
Danrealbe <odeboy5518@gmail.com>
2023-10-07 05:43:14 -0700

    tuple

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
  # Daneil Ben
 
-
- def multiple_returns(sentence):
-     """Returns the length of a string and its first character."""
-     first = "None" if len( sentence) == 0 else sentence[0]
-     new_tuple = (len(sentence), first)
-     return new_tuple
+def multiple_returns(sentence):
+    first = "None" if len(sentence) == 0 else sentence[0]
+    new_tuple = (len(sentence), first)
+    return new_tuple
 
