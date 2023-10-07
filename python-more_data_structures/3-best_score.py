8100c895430dc7880759b6965b997aab8f514a0c
Danrealbe <odeboy5518@gmail.com>
2023-10-07 09:26:18 -0700

    best score

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if a_dictionary:
         a = sorted((value, key)
-            for (key, value) in a_dictionary.items()).pop()[1]
-    return (a)
+                   for (key, value) in a_dictionary.items()).pop()[1]
+        return (a)
